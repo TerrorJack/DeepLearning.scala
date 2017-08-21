e4e8a51bdb40c7b3be6f631fb8580379d7114ca2
Shao Cheng <astrohavoc@gmail.com>
2017-08-21 15:12:50 +0800

    A working implementation of RemoteDoAkka plugin.

--- plugins-RemoteDoAkka/src/main/scala/com/thoughtworks/deeplearning/plugins/RemoteDoAkka.scala
+++ plugins-RemoteDoAkka/src/main/scala/com/thoughtworks/deeplearning/plugins/RemoteDoAkka.scala
@@ -1,22 +1,61 @@
 package com.thoughtworks.deeplearning.plugins
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
 import java.util.concurrent.atomic.AtomicInteger
-
-import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
-
-import scalaz.syntax.all._
+import scala.concurrent.SyncVar
+import scala.collection.parallel.mutable.ParHashMap
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+import akka.routing.{BalancingPool, Router}
+import com.thoughtworks.future._
 import com.thoughtworks.raii.asynchronous._
-import com.thoughtworks.feature.Factory.inject
+
+private class RemoteDoActor(context: RemoteDoAkka) extends Actor {
+  import RemoteDoAkka._
+
+  def receive = {
+    case IndexedDoConstructor(doConstructor, ref) =>
+      val result = doConstructor().run.blockingAwait
+      sender() ! IndexedResult(result, ref)
+
+    case IndexedResult(result, ref) =>
+      context.remoteDoResultMap(ref).put(result)
+  }
+}
+
+object RemoteDoAkka {
+  type RemoteDoRef = Int
+
+  final case class IndexedDoConstructor[A](doConstructor: () => Do[A], ref: RemoteDoRef)
+  final case class IndexedResult[A](result: A, ref: RemoteDoRef)
+}
 
 trait RemoteDoAkka extends RemoteDo {
 
+  import RemoteDoAkka._
+
   protected val remoteDoRefSupply = new AtomicInteger(0)
+
+  // For communication between RemoteDoActor. Should really be protected, but..
+  val remoteDoResultMap: ParHashMap[RemoteDoRef, SyncVar[Any]] = ParHashMap.empty
 
   val akkaActorSystem: ActorSystem
 
+  protected val balancingPool = BalancingPool(8)
+
+  protected lazy val localRemoteDoActor: ActorRef =
+    akkaActorSystem.actorOf(Props(new RemoteDoActor(this)), "localRemoteDoActor")
+
+  protected lazy val balancingPoolRouter: Router =
+    balancingPool.createRouter(akkaActorSystem).addRoutee(localRemoteDoActor)
+
   override def runDo[A](doA: => Do[A]): Do[A] = {
-    Do.execute(doA)(akkaActorSystem.dispatcher).join
+    val ref = remoteDoRefSupply.getAndIncrement
+    remoteDoResultMap.update(ref, new SyncVar())
+    balancingPoolRouter.route(IndexedDoConstructor(doA _, ref), localRemoteDoActor)
+    Do.delay {
+      val result = remoteDoResultMap(ref).take
+      remoteDoResultMap.remove(ref)
+      result.asInstanceOf[A]
+    }
   }
 
 }
